4a3b45af0ead8f63d30fa5004922526787dfe21d
Wasja Bloch <wasja@norsar.no>
2025-08-26 12:55:58 +0200

    Corrected all Amplitdue objects

--- tests/test_qc.py
+++ tests/test_qc.py
@@ -115,8 +115,8 @@ def test_index_high_value():
 
 def test_clean_by_station():
     amplitudes = [
-        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA2", 0, 1, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.8, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA2", 0, 1, 1.0, 0.1, 0.8, 0.9, 0.1, 0.5, 20.0),
     ]
     result = qc.clean_by_station(amplitudes, ["STA1"])
     assert len(result) == 1
@@ -125,8 +125,8 @@ def test_clean_by_station():
 
 def test_clean_by_misfit():
     amplitudes = [
-        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA2", 0, 1, 1.0, 1.5, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.8, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA2", 0, 1, 1.0, 1.5, 0.8, 0.9, 0.1, 0.5, 20.0),
     ]
     result = qc.clean_by_misfit(amplitudes, 1.0)
     assert len(result) == 1
@@ -135,8 +135,8 @@ def test_clean_by_misfit():
 
 def test_clean_by_event_p():
     amplitudes = [
-        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA2", 0, 2, 1.0, 1.5, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.8, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA2", 0, 2, 1.0, 1.5, 0.8, 0.9, 0.1, 0.5, 20.0),
     ]
     result = qc.clean_by_event(amplitudes, [2])
     assert len(result) == 1
@@ -146,10 +146,10 @@ def test_clean_by_event_p():
 def test_clean_by_event_s():
     amplitudes = [
         core.S_Amplitude_Ratios(
-            "STA1", 0, 1, 2, 1.0, 1.0, 0.1, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 1, 2, 1.0, 1.0, 0.1, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
         core.S_Amplitude_Ratios(
-            "STA2", 0, 2, 3, 1.0, 1.0, 1.5, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA2", 0, 2, 3, 1.0, 1.0, 1.5, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
     ]
     result = qc.clean_by_event(amplitudes, [3])
@@ -164,8 +164,8 @@ def test_clean_by_magnitude_difference_p():
         core.Event(0, 0, 0, 0, 7, "Ev1"),
     ]
     amplitudes = [
-        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA2", 0, 2, 1.0, 1.5, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.7, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA2", 0, 2, 1.0, 1.5, 0.7, 0.9, 0.1, 0.5, 20.0),
     ]
     result = qc.clean_by_magnitude_difference(amplitudes, events, 1.5)
     assert len(result) == 1
@@ -182,10 +182,10 @@ def test_clean_by_magnitude_difference_s():
 
     amplitudes = [
         core.S_Amplitude_Ratios(
-            "STA1", 0, 1, 2, 1.0, 1.0, 0.1, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 1, 2, 1.0, 1.0, 0.1, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
         core.S_Amplitude_Ratios(
-            "STA2", 0, 1, 3, 1.0, 1.0, 0.1, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA2", 0, 1, 3, 1.0, 1.0, 0.1, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
     ]
 
@@ -203,8 +203,8 @@ def test_clean_by_valid_takeoff_angle():
     }
 
     amplitudes = [
-        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA1", 0, 2, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.7, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 2, 1.0, 0.1, 0.7, 0.9, 0.1, 0.5, 20.0),
     ]
 
     result = qc.clean_by_valid_takeoff_angle(amplitudes, phd)
@@ -216,10 +216,10 @@ def test_clean_by_valid_takeoff_angle():
 def test_clean_by_kurtosis_p():
 
     amplitudes = [
-        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA1", 0, 2, 1.1, 1.5, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA1", 0, 3, 1.2, 1.5, 0.9, 0.1, 0.5, 20.0),
-        core.P_Amplitude_Ratio("STA1", 1, 3, 1e13, 1.5, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 1, 1.0, 0.1, 0.7, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 2, 1.1, 1.5, 0.7, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 0, 3, 1.2, 1.5, 0.7, 0.9, 0.1, 0.5, 20.0),
+        core.P_Amplitude_Ratio("STA1", 1, 3, 1e13, 1.5, 0.7, 0.9, 0.1, 0.5, 20.0),
     ]
 
     events = [
@@ -238,19 +238,19 @@ def test_clean_by_kurtosis_s():
 
     amplitudes = [
         core.S_Amplitude_Ratios(
-            "STA1", 0, 1, 2, 1.0, 1.0, 0.1, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 1, 2, 1.0, 1.0, 0.1, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
         core.S_Amplitude_Ratios(
-            "STA1", 0, 1, 3, 1.1, 1.1, 1.5, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 1, 3, 1.1, 1.1, 1.5, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
         core.S_Amplitude_Ratios(
-            "STA1", 0, 1, 4, 1.2, 1e-13, 1.5, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 1, 4, 1.2, 1e-13, 1.5, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
         core.S_Amplitude_Ratios(
-            "STA1", 0, 2, 4, 1.1, 1.1e-13, 1.5, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 2, 4, 1.1, 1.1e-13, 1.5, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),
         core.S_Amplitude_Ratios(
-            "STA1", 0, 3, 4, 0.9, 0.9, 1.5, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 3, 4, 0.9, 0.9, 1.5, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         ),  # exclude me
     ]
 
@@ -268,7 +268,7 @@ def test_clean_by_kurtosis_s():
 
 
 def test_ps_amplitudes_p():
-    pamps = [core.P_Amplitude_Ratio("STA1", 0, 1, 0.1, 0.1, 0.9, 0.1, 0.5, 20.0)]
+    pamps = [core.P_Amplitude_Ratio("STA1", 0, 1, 0.1, 0.1, 0.7, 0.9, 0.1, 0.5, 20.0)]
 
     ip, _ = qc._ps_amplitudes(pamps)
     assert ip
@@ -277,7 +277,7 @@ def test_ps_amplitudes_p():
 def test_ps_amplitudes_s():
     samps = [
         core.S_Amplitude_Ratios(
-            "STA1", 0, 1, 3, 0.1, 0.2, 0.1, 0.8, 0.2, 0.0, 0.5, 20.0
+            "STA1", 0, 1, 3, 0.1, 0.2, 0.1, 0.7, 0.8, 0.2, 0.0, 0.5, 20.0
         )
     ]
     ip, _ = qc._ps_amplitudes(samps)
